493cfd379289855a9947fd892bfb89aaf4c167a6
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-06-22 00:08:39 +0300

    hw03-manDB-node

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 require('colors');
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://Oleksandr:SucCubus888@cluster0.v8lgymb.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
